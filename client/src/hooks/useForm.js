0ca4c42b3c67163c2979d5f6bf240c656627a263
Matthew Serwer <Matthew.Serwer@gmail.com>
2020-10-30 11:44:10 -0400

    the plants are rendering when PlantList is mounted

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,19 @@
-// write your custom hook here to control your checkout form
+import React, { useState } from 'react';
+
+export const useForm = (initialValues) => {
+    const [value, setValue] = useState([]);
+
+    const handleChanges = e => {
+        setValue({
+            ...value,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const clearForm = e => {
+        e.preventDefault();
+        setValue(initialValues)
+    }
+
+    return([value, handleChanges, clearForm]);
+}
